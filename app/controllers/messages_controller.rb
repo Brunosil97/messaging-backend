8c1f91a83027fe6d4f6094c938a1c43c826c663e
Amy Franz <amy.franz@gmail.com>
2020-04-15 16:26:39 +0100

    create message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,5 +3,15 @@ class MessagesController < ApplicationController
     def index 
         messages = Message.all
         render json: messages
+    end
+    def create
+        messages = Message.create(message_params)
+        render json: messages
     end 
+
+    private 
+
+    def message_params
+        params.require(:message).permit(:content, :user_id, :chat_id)
+    end
 end
