3a2d1b8cab717085de88fd30e5f2ba3fb8ace552
Amy Franz <amy.franz@gmail.com>
2020-04-15 10:53:06 +0100

    merge

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
     skip_before_action :authorize_user
+    skip_before_action :verify_authenticity_token
     
     def index 
         users = User.all
@@ -12,7 +13,7 @@ class UsersController < ApplicationController
             sessions[:user_id] = @user.id
             redirect_to controller: 'sessions', action: 'new'
         else 
-            flash[:errors] = "Cannot create user"
+            # flash[:errors] = "Cannot create user"
             redirect_to controller: 'users', action: 'new'
         end
     end
@@ -20,6 +21,6 @@ class UsersController < ApplicationController
     private 
 
     def user_params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation, :profileImage)
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
