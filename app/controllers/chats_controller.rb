aef9eaae1f0d134daaf52c666ed657627f417968
Bruno Silva <brunosilvinosilva@gmail.com>
2020-04-16 09:43:48 +0100

    added create routes

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -2,10 +2,17 @@ class ChatsController < ApplicationController
 
     def index 
         chats = Chat.all
-        render json: chats.to_json(:include => {:users => {:only => [:name, :email]}})
+        render json: chats.to_json(:include => {:users => {:only => [:name, :email]}, :messages => {:only => [:content, :user_id]}})
     end
+
+    def create
+        chat = Chat.create(hasRead: params[:hasRead])
+        render json: chat
+    end 
+
+
     def show
         chat = Chat.find_by(id: params[:id])
-        render json: chat.to_json(:include => {:users => {:only => [:name, :email]}})
+        render json: chat.to_json(:include => {:users => {:only => [:name, :email]}, :messages => {:only => [:content, :user_id]}})
     end
 end
--- app/controllers/user_in_chats_controller.rb
+++ app/controllers/user_in_chats_controller.rb
@@ -4,4 +4,15 @@ class UserInChatsController < ApplicationController
         userchats = UserInChat.all
         render json: userchats
     end 
+
+    def create 
+        userinchat = UserInChat.create(userinchat_params)
+        render json: userinchat
+    end 
+
+    private 
+
+    def userinchat_params 
+        params.require(:user_in_chat).permit(:chat_id, :user_id)
+    end
 end
